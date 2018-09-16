acba31a9375825bcb32ac2f5638280c8b57ab0cc
Brusnitsyna <42172367+Brusnitsyna@users.noreply.github.com>
2018-09-16 20:12:42 +0300

    Верстка со скриптом

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var searchHotelsBtn = document.querySelector(".search-hotels-button");
+var form = document.querySelector(".search-form");
+var entryInput = form.querySelector("[name=date-of-entry]");
+var departureInput = form.querySelector("[name=date-of-departure]");
+var adultInput = form.querySelector("[name=number-of-adults]");
+var childrenInput = form.querySelector("[name=number-of-children]");
+
+searchHotelsBtn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.toggle("js-hidden");
+  form.classList.remove("search-form-error");
+  });
+
+form.addEventListener("submit", function (evt) {
+    if ( !entryInput.value || !departureInput.value || !adultInput.value || !childrenInput.value) {
+      evt.preventDefault();
+      form.classList.add("search-form-error");
+      }
+      });
